75bafea098ed72b5e41660a1652035bbeb8d4254
mfazari <massimofazari@bluewin.ch>
2019-10-22 17:13:55 +0200

    way added

--- src/Train_simulator.java
+++ src/Train_simulator.java
@@ -3,9 +3,11 @@ import Stations.Station;
 import Vehicle.Train;
 import Vehicle.Train_1;
 
+import java.util.concurrent.TimeUnit;
+
 public class Train_simulator {
 
-    public static void simulate(){
+    public static void simulate() throws InterruptedException {
 
         // Stations
         Station London = new Station("London", rand.nextInt(1000));
@@ -17,11 +19,27 @@ public class Train_simulator {
 
         // Simulation
         Train_1 train = new Train_1(London_Birmingham);
-        Train
-
-
+        int commute_time = train.commute_time;
+        go(commute_time);
 
     }
 
+    public static void go(int time) throws InterruptedException {
+
+        TimeUnit.SECONDS.sleep(time);
+
+        //train returns after waiting for 5 seconds
+        TimeUnit.SECONDS.sleep(5);
+        back(time);
+
+    }
+
+    public static void back(int time) throws InterruptedException {
+        TimeUnit.SECONDS.sleep(time);
+
+        //train returns after waiting for 5 seconds
+        TimeUnit.SECONDS.sleep(5);
+        go(time);
+    }
 
 }
--- src/Vehicle/Train_1.java
+++ src/Vehicle/Train_1.java
@@ -5,18 +5,20 @@ import Paths.Route;
 public class Train_1 extends Train {
 
     // TODO implement train
-    int passenger_number;
+    int space;
     public int max_speed;
     int current_speed;
-    int commute_time;
+    public int commute_time;
     Route route;
 
     public Train_1(Route route){
-        this.passenger_number = 400;
+        this.space = 400;
         this.max_speed = 120;
         this.route = route;
         this.current_speed = route.getSpeed(max_speed);
         this.commute_time = route.commute_time(current_speed);
     }
+
+
 
 }
